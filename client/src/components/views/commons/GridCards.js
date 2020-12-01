c8e86f62211738b7fe8f45eafcf96442be1406a4
your Carrot <stylenbs@gmail.com>
2020-12-01 20:29:36 +0900

    props 조건에 따른 return 변경

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -3,15 +3,25 @@ import { Col } from "antd";
 import "./GridCards.css";
 
 function GridCards(props) {
-  return (
-    <Col lg={6} md={8} xs={24}>
-      <div className="grid-cards">
-        <a href={`/movie/${props.movieId}`}>
-          <img src={props.image} alt={props.movieName} />
-        </a>
-      </div>
-    </Col>
-  );
+  if (props.landingPage) {
+    return (
+      <Col lg={6} md={8} xs={24}>
+        <div className="grid-cards">
+          <a href={`/movie/${props.movieId}`}>
+            <img src={props.image} alt={props.movieName} />
+          </a>
+        </div>
+      </Col>
+    );
+  } else {
+    return (
+      <Col lg={6} md={8} xs={24}>
+        <div className="grid-cards">
+          <img src={props.image} alt={props.profileName} />
+        </div>
+      </Col>
+    );
+  }
 }
 
 export default GridCards;
